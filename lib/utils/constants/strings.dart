ba4bae798b3fa031ae052c985c313fa40924da5c
prajwalvasista <112843783+prajwalvasista@users.noreply.github.com>
2024-03-11 17:19:20 +0530

    Common Strings

---
+++ lib/utils/constants/strings.dart
@@ -0,0 +1,32 @@
+class CommonStrings {
+  static const verification = "GST Verification";
+  static const conformationGst = "I have a GST no.";
+  static const validationGst = "Please enter GST number";
+  static const correctGstMessage = "Please enter correct GST number";
+  static const gstHintText = "Enter GST no.";
+  static const dontHaveGst = "I don't have a GST no.";
+  static const panNumber = "PAN No.";
+  static const askingPanNumber = "Enter Your PAN No";
+  static const validationPanNumber = "Enter Your PAN Number";
+  static const validationPanNumber1 = "Enter correct PAN Number";
+  static const askingPanDoc = "Upload PAN document";
+  static const panText = "Upload";
+  static const storeName = "Store Name";
+  static const askingStoreName = "Enter Your Store Name";
+  static const pickUpAddress = "PickUp Address";
+  static const pinCode = "Pin Code";
+  static const validatePinCode = "Enter Pin code";
+  static const enterCorrectPinCode = "Enter correct pin code";
+  static const city = "City";
+  static const enterCityName = "Enter City name";
+  static const enterCorrectCity = "Please enter correct city";
+  static const state = "State";
+  static const areaStreet = "Area,Street,Building no.";
+  static const chooseShippingPreference = "Choose Shipping Preferences";
+  static const rovaStore = "You Store,Rova Shipping(Easy ship)";
+  static const packOrders =
+      "You store and pack orders at you location.\n We pick them and deliver to customers";
+  static const easyShip = "You Store,(Easy ship)";
+  static const packOrdersOne =
+      "You store and pack orders at you location.\n We pick them and deliver to customers";
+}
